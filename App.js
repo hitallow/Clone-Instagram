5984f80cc381f791cb990ed8aaab19cef683d3d0
Hitallo William <hitallo91@gmail.com>
2019-05-28 19:55:29 -0300

    component post

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
 import Header from './src/components/Header'
+import Post from './src/components/Post'
+import Image from './assets/imgs/fence.jpg'
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <Header/> 
+      <View style={styles.container}>
+        <Header />
+        <Post image={Image} />
+        <Post image={Image} />
+      </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
 
 
